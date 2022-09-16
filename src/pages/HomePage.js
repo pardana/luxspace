dc748855ba4c2208db21992355c29201f6b2caa0
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-17 00:31:18 +0700

    create section client and sitemap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,8 @@ import Header from "parts/Header";
 import Hero from "parts/Hero";
 import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
+import Clients from "parts/Clients";
+import Sitemap from "parts/Sitemap";
 
 export default function HomePage(props) {
   return (
@@ -12,6 +14,8 @@ export default function HomePage(props) {
       <Hero />
       <BrowseRoom />
       <JustArrived />
+      <Clients />
+      <Sitemap />
     </>
   );
 }
