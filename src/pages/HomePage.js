8e1857e4bbd76b491f02e2ea7f7310291af34f0a
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-17 00:16:56 +0700

    create section BrowseRoom

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,8 @@
 import React from "react";
+
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
 
 export default function HomePage(props) {
@@ -8,6 +10,7 @@ export default function HomePage(props) {
     <>
       <Header />
       <Hero />
+      <BrowseRoom />
       <JustArrived />
     </>
   );
