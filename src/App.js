14c6aa2cbd5c8fa3887f39af51504cad2267c663
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-25 01:12:33 +0700

    update Switch Router DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "assets/css/app.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import HomePage from "pages/HomePage";
 import Details from "pages/Details";
@@ -11,11 +11,13 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/categories/:idc" component={Details} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/congratulation" component={Congratulation} />
-        <Route path="*" component={NotFound} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/categories/:idc" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/congratulation" component={Congratulation} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </Router>
     </div>
   );
