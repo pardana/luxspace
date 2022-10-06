1a8f4016b4abdb8c114786b777d2be4efcafa971
Aditria Pardana <aditria.pardana20@gmail.com>
2022-10-06 15:01:39 +0700

    update browse room responsive

--- src/helpers/fetch/index.js
+++ src/helpers/fetch/index.js
@@ -2,11 +2,13 @@ export default function fetchData({
   url,
   method = "GET",
   host = process.env.REACT_APP_API_HOST,
+  body
 }) {
   return fetch(`${host}${url}`, {
     method,
     mode: "cors",
     headers: { "Content-Type": "application/json" },
+    body
   }).then(async (response) => {
     const jsonResponse = await response.json();
     if (response.ok) {
