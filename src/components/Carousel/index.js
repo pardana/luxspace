73cc8154fec1010fc01055b5c45d7ce0c38da7bd
Aditria Pardana <aditria.pardana20@gmail.com>
2022-10-13 16:41:40 +0700

    update carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useCallback, useLayoutEffect } from "react";
+import { addClass, removeClass } from "helpers/format/classNameModifier";
 
 export default function Carousel({ children, refContainer }) {
   const refDragHandler = useRef(null);
@@ -15,21 +16,138 @@ export default function Carousel({ children, refContainer }) {
   const posFinal = useRef();
   const isAllowShift = useRef(true);
   const cards = useRef();
+  const cardSize = cards.current?.[0].offsetWidth || 0;
+  const cardCount = cards.current?.length || 0;
 
-  const onDragMove = useCallback((e) => {
-    e = e || window.event;
-    console.log(e);
-  }, []);
+  const fnCheckIndex = useCallback(() => {
+    if (e.propertyName === "left") {
+      setTimeout(() => {
+        removeClass(refDragHandler.current, "transition-all duration-200");
+      }, 200);
 
-  const onDragEnd = useCallback((e) => {
-    e = e || window.event;
-    console.log(e);
-  }, []);
+      const isMobile = window.innerWidth < 767 ? 0 : -1;
+      if (index <= 0) {
+        refDragHandler.current.style.left = 0;
+        setIndex(0);
+      } else if (index >= cardCount - itemToShow) {
+        refDragHandler.current.style.left = -(
+          (cardCount - itemToShow + isMobile) *
+          cardSize
+        );
+        setIndex(cardCount - itemToShow);
+      } else if (index === cardCount || index === cardCount - 1) {
+        refDragHandler.current.style.left = (cardCount - 1) * cardSize;
+        setIndex(cardCount - 1);
+      }
 
-  const onDragStart = useCallback((e) => {
+      isAllowShift.current = true;
+    }
+  }, [cardCount, cardSize, index, itemToShow]);
+
+  const fnShiftItem = useCallback(
+    (direction) => {
+      addClass(refDragHandler.current, "transition-all duration-200");
+
+      if (isAllowShift.current) {
+        if (direction === "DIRECTION_LEFT") {
+          setIndex((prev) => prev + 1);
+          refDragHandler.current.style.left = `${
+            posInitial.current - cardSize
+          }px`;
+        } else if (direction === "DIRECTION_RIGHT") {
+          setIndex((prev) => prev - 1);
+          refDragHandler.current.style.left = `${
+            posInitial.current + cardSize
+          }px`;
+        }
+      }
+
+      isAllowShift.current = false;
+    },
+    [cardSize]
+  );
+
+  const onDragMove = useCallback(
+    (e) => {
+      e = e || window.event;
+      e.preventDefault();
+
+      if (e.type === "touchmove") {
+        posX2.current = posX1.current - e.touches[0].clientX;
+        posX1.current = e.touches[0].clientX;
+      } else {
+        posX2.current = posX1.current - e.clientX;
+        posX1.current = e.clientX;
+      }
+
+      refDragHandler.current.style.left = `${
+        refDragHandler.current.offsetLeft - posX2.current
+      }px`;
+    },
+    [posX1, posX2]
+  );
+
+  const onDragEnd = useCallback(
+    (e) => {
+      e = e || window.event;
+      e.preventDefault();
+
+      posFinal.current = refDragHandler.current.offsetLeft;
+
+      if (posFinal.current - posInitial.current < -threshold) {
+        fnShiftItem(DIRECTION_LEFT);
+      } else if (posFinal.current - posInitial.current > threshold) {
+        fnShiftItem(DIRECTION_RIGHT);
+      } else {
+        refDragHandler.current.style.left = `${posInitial.current}px`;
+      }
+
+      document.onmouseup = null;
+      document.onmousemove = null;
+    },
+    [fnShiftItem]
+  );
+
+  const onDragStart = useCallback(
+    (e) => {
+      e = e || window.event;
+      e.preventDefault();
+
+      posInitial = refDragHandler.current.offsetLeft;
+
+      if (e.type === "touchstart") {
+        posX1.current = e.touches[0].clientX;
+      } else {
+        posX1.current = e.clientX;
+        document.onmouseup = onDragEnd;
+        document.onmousemove = onDragMove;
+      }
+    },
+    [onDragEnd, onDragMove]
+  );
+
+  const onClick = (e) => {
     e = e || window.event;
-    console.log(e);
-  }, []);
+    !isAllowShift.current && e.preventDefault();
+  };
+
+  useLayoutEffect(() => {
+    const refForwardDragHandler = refDragHandler.current;
+
+    refForwardDragHandler.onmousedown = onDragStart;
+    refForwardDragHandler.addEventListener("touchstart", onDragStart);
+    refForwardDragHandler.addEventListener("touchstart", onDragMove);
+    refForwardDragHandler.addEventListener("touchmove", onDragEnd);
+    refForwardDragHandler.addEventListener("click", onClick);
+    refForwardDragHandler.addEventListener("transitionend", fnCheckIndex);
+    return () => {
+      refForwardDragHandler.removeEventListener("touchstart", onDragStart);
+      refForwardDragHandler.removeEventListener("touchstart", onDragMove);
+      refForwardDragHandler.removeEventListener("touchmove", onDragEnd);
+      refForwardDragHandler.addEventListener("click", onClick);
+      refForwardDragHandler.addEventListener("transitionend", fnCheckIndex);
+    };
+  }, [onDragStart, onDragMove, onDragEnd, onClick, fnCheckIndex]);
 
   return (
     <div ref={refDragHandler} className="flex -mx-4 flex-row relative">
