946e0dcf6c30ff9ebde5d29f5e7e35fba3d9a1ba
Aditria Pardana <aditria.pardana20@gmail.com>
2022-10-18 14:54:05 +0700

    update loading skeleton suggestion

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -66,6 +66,45 @@ function LoadingProductDetails() {
   );
 }
 
+function LoadingSuggestion() {
+  return (
+    <section className="bg-gray-100 px-4 py-16">
+      <div className="container mx-auto">
+        <div className="flex flex-start mb-4">
+          <h3 className="text-2xl capitalize font-semibold">
+            Complete your room <br className="" />
+            with what we designed
+          </h3>
+        </div>
+        <div className="flex overflow-x-auto mb-4 -mx-3">
+          {Array(4)
+            .fill()
+            ?.map((_, index) => {
+              return (
+                <div
+                  className="px-3 flex-none"
+                  style={{ width: "320px" }}
+                  key={index}
+                >
+                  <div className="rounded-xl p-4 pb-8 relative bg-white">
+                    <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
+                      <div
+                        className="item bg-gray-300 animate-pulse rounded-lg h-full overflow-hidden"
+                        style={{ width: 287, height: 150 }}
+                      ></div>
+                    </div>
+                    <div className="w-56 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+                    <div className="w-40 h-4 mt-3 mb-3 bg-gray-300 animate-pulse rounded-full"></div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default function HomePage() {
   const { idp } = useParams();
 
@@ -86,14 +125,13 @@ export default function HomePage() {
         ]}
       />
 
-      {/* <LoadingProductDetails /> */}
       {isLoading ? <LoadingProductDetails /> : <ProductDetails data={data} />}
 
-      {/* {isLoading ? (
-        "Loading..."
+      {isLoading ? (
+        <LoadingSuggestion />
       ) : (
         <Suggestion data={data?.relatedProducts || {}} />
-      )} */}
+      )}
 
       <Clients />
       <Sitemap />
