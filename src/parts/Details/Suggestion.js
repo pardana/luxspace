89c78ee24d608d187bb04555248db0703093d755
Aditria Pardana <aditria.pardana20@gmail.com>
2022-10-18 14:07:36 +0700

    update suggeestion

--- src/parts/Details/Suggestion.js
+++ src/parts/Details/Suggestion.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Suggestion() {
+export default function Suggestion({ data }) {
   return (
     <>
       {/* START: COMPLETE YOUR ROOM  */}
@@ -14,62 +14,31 @@ export default function Suggestion() {
             </h3>
           </div>
           <div className="flex overflow-x-auto mb-4 -mx-3">
-            <div className="px-3 flex-none" style={{width: "320px"}}>
-              <div className="rounded-xl p-4 pb-8 relative bg-white">
-                <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                  <img
-                    src="/images/content/chair-1.png"
-                    alt=""
-                    className="w-full h-full object-cover object-center"
-                  />
+            {data?.map((item) => {
+              return (
+                <div
+                  className="px-3 flex-none"
+                  style={{ width: "320px" }}
+                  key={item.id}
+                >
+                  <div className="rounded-xl p-4 pb-8 relative bg-white">
+                    <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
+                      <img
+                        src={item.imageUrl}
+                        alt={item.title}
+                        className="w-full h-full object-cover object-center"
+                      />
+                    </div>
+                    <h5 className="text-lg font-semibold mt-4">{item.title}</h5>
+                    <span className="">IDR {item.price}</span>
+                    <Link
+                      to={`/categories/${item.idc}/products/${item.id}`}
+                      className="stretched-link"
+                    ></Link>
+                  </div>
                 </div>
-                <h5 className="text-lg font-semibold mt-4">Cangkir Mauttie</h5>
-                <span className="">IDR 89.300.000</span>
-                <Link to="details.html" className="stretched-link"></Link>
-              </div>
-            </div>
-            <div className="px-3 flex-none" style={{width: "320px"}}>
-              <div className="rounded-xl p-4 pb-8 relative bg-white">
-                <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                  <img
-                    src="/images/content/chair-2.png"
-                    alt=""
-                    className="w-full h-full object-cover object-center"
-                  />
-                </div>
-                <h5 className="text-lg font-semibold mt-4">Saman Kakka</h5>
-                <span className="">IDR 14.500.399</span>
-                <Link to="details.html" className="stretched-link"></Link>
-              </div>
-            </div>
-            <div className="px-3 flex-none" style={{width: "320px"}}>
-              <div className="rounded-xl p-4 pb-8 relative bg-white">
-                <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                  <img
-                    src="/images/content/chair-3.png"
-                    alt=""
-                    className="w-full h-full object-cover object-center"
-                  />
-                </div>
-                <h5 className="text-lg font-semibold mt-4">Lino Dino</h5>
-                <span className="">IDR 22.000.000</span>
-                <Link to="details.html" className="stretched-link"></Link>
-              </div>
-            </div>
-            <div className="px-3 flex-none" style={{width: "320px"}}>
-              <div className="rounded-xl p-4 pb-8 relative bg-white">
-                <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                  <img
-                    src="/images/content/chair-1.png"
-                    alt=""
-                    className="w-full h-full object-cover object-center"
-                  />
-                </div>
-                <h5 className="text-lg font-semibold mt-4">Syail Ammeno</h5>
-                <span className="">IDR 6.399.999</span>
-                <Link to="details.html" className="stretched-link"></Link>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </section>
